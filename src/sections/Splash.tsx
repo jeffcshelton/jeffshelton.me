b226a3d0ec49a74194f729118a00a9821cc30ae2
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-09-07 14:21:19 -0400

    Add text & layout styling to Splash

--- src/sections/Splash.tsx
+++ src/sections/Splash.tsx
@@ -11,15 +11,15 @@ const Splash: React.FC = () => {
 			</div>
 			<div className="col d-flex flex-column flex-nowrap">
 				<nav className="d-flex justify-content-evenly align-items-center mt-4">
-					<a className="fw-bold hover-underline" href="#about">About</a>
-					<a className="fw-bold hover-underline" href="#skills">Skills</a>
-					<a className="fw-bold hover-underline" href="#experience">Experience</a>
-					<a className="btn btn-primary fw-bold shadow" href={resume}>Resume</a>
+					<a className="text-dark fw-bold text-decoration-none hover-underline" href="#about">About</a>
+					<a className="text-dark fw-bold text-decoration-none hover-underline" href="#skills">Skills</a>
+					<a className="text-dark fw-bold text-decoration-none hover-underline" href="#experience">Experience</a>
+					<a className="btn btn-primary text-light fw-bold shadow" href={resume}>Resume</a>
 				</nav>
 				<div className="d-flex align-items-center flex-grow-1">
 					<div>
-						<h1 className="text-secondary fw-light">Hi, I'm <span className="text-dark fw-bold">Jeff Shelton</span>.</h1>
-						<h2 className="text-secondary fw-light">I make <span className="text-primary fw-bolder"><strong>software</strong></span>.</h2>
+						<h1 className="fw-light">Hi, I'm <span className="text-dark fw-bold">Jeff Shelton</span>.</h1>
+						<h2 className="fw-light">I make <span className="text-primary fw-bolder"><strong>software</strong></span>.</h2>
 					</div>
 				</div>
 			</div>
