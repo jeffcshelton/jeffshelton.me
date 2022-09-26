95ff91658c2e9d820183b1d423fa012accbcdcb6
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-09-26 00:45:17 -0400

    Fix spacing in WIP statement

--- src/sections/WIP.tsx
+++ src/sections/WIP.tsx
@@ -4,8 +4,10 @@ import resume from "../../res/resume.pdf";
 const WIP: React.FC = () => {
 	return (
 		<section className="vh-100 d-flex align-items-center justify-content-center">
-			<h3 className="w-75 text-center">This site is still a work-in-progess. Check out <a href="https://github.com/jeffcshelton">my GitHub</a> or <a href={resume}>my resume</a> to see all the cool stuff I'm working on!</h3>
-			<h3 className="w-75 text-center">Even check out the <a href="https://github.com/jeffcshelton/jeffshelton.me">source code</a> for this website!</h3>
+			<div className="w-75">
+				<h3 className="text-center">This site is still a work-in-progess. Check out <a href="https://github.com/jeffcshelton">my GitHub</a> or <a href={resume}>my resume</a> to see all the cool stuff I'm working on!</h3>
+				<h3 className="text-center">Even check out the <a href="https://github.com/jeffcshelton/jeffshelton.me">source code</a> for this website!</h3>
+			</div>
 		</section>
 	)
 };
